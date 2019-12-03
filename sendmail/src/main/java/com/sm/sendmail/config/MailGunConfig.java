3a6c7e96b524db1a6d5dc05eaef122c533ab9c0b
hnarayanan <hnarayanan@SYDL85430.iis.amadeus.net>
2019-12-03 13:59:58 +1100

    Obtain the mailgun domain from env or system properties

--- sendmail/src/main/java/com/sm/sendmail/config/MailGunConfig.java
+++ sendmail/src/main/java/com/sm/sendmail/config/MailGunConfig.java
@@ -10,15 +10,41 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties("mailgun")
 public class MailGunConfig {
 	
-	private String url;
+	private String urlPfx;
+	
+	private String urlSfx;
+	
+	public String getUrlPfx() {
+		return urlPfx;
+	}
+	
+	public void setUrlPfx(String urlPfx) {
+		this.urlPfx = urlPfx;
+	}
+	
+	public String getUrlSfx() {
+		return urlSfx;
+	}
+	
+	public void setUrlSfx(String urlSfx) {
+		this.urlSfx = urlSfx;
+	}
 	
 	public String getUrl() {
+		String domain = System.getenv("MAILGUN_DOMAIN");
+		if(domain == null) {
+			// try to check for system properties for the api key
+			domain = System.getProperty("mailgun.domain");
+		}
+		if(domain == null || domain.trim().isEmpty()) {
+			System.out.println("Domain for Mailgun is not configured properly!!");
+			return "";
+		}
+		String url = urlPfx + domain + urlSfx;
+		System.out.println("Mailgun url = " + url);
 		return url;
 	}
 	
-	public void setUrl(String url) {
-		this.url = url;
-	}
 	
 	public String getApiKey() {
 		String apiKey = System.getenv("MAILGUN_APIKEY");
